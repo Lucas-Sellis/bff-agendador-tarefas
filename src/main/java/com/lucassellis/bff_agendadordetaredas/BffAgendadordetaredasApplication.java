428a67387471e5640863bdff098b6902f44cb3ad
Lucas <lucas.sellis@gmail.com>
2026-01-28 07:09:48 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/lucassellis/bff_agendadordetaredas/BffAgendadordetaredasApplication.java
@@ -0,0 +1,13 @@
+package com.lucassellis.bff_agendadordetaredas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadordetaredasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadordetaredasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucassellis/bff_agendadordetaredas/BffAgendadordetaredasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassellis.bff_agendadordetaredas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadordetaredasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
