6334ff540ec939ec1fe7556a84f393256d5d457e
Lucas <lucas.sellis@gmail.com>
2026-01-30 09:17:24 -0300

    adicionado client de email

---
+++ src/main/java/com/lucassellis/bff_agendadordetarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,15 @@
+package com.lucassellis.bff_agendadordetarefas.infrastructure.client;
+
+import com.lucassellis.bff_agendadordetarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+
+
+}
